0eb1eb7b31cc4274249479a926bd8b56bea8b270
celoplay <celoaizaga@gmail.com>
2025-08-11 23:56:59 +0200

    paginacion

--- src/app/components/list-cocktails/list-cocktails.ts
+++ src/app/components/list-cocktails/list-cocktails.ts
@@ -3,16 +3,23 @@ import { FormsModule } from '@angular/forms';
 import { IFilter } from '../../models/filter.model';
 import { CocktailService } from '../../services/cocktail-service';
 import { ICocktail } from '../../models/cocktail.model';
+import { RouterLink } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-list-cocktails',
-  imports: [FormsModule],
+  imports: [FormsModule,RouterLink, NgxPaginationModule],
   templateUrl: './list-cocktails.html',
   styleUrl: './list-cocktails.scss',
 })
 export class ListCocktails {
   private cocktailService = inject(CocktailService);
   public listCocktails: ICocktail[] = [];
+  public searched: boolean = false;
+  public loadCocktails: boolean = true;
+
+  public currentPage = 1;
+  public itemsPerPage = 12;
 
   public filter: IFilter = {
     searchBy: 'name',
@@ -20,6 +27,8 @@ export class ListCocktails {
   };
 
   filterData() {
+    this.searched = true;
+    this.loadCocktails = false;
     this.cocktailService.getCocktails(this.filter).subscribe({
       next: (cocktails: ICocktail[]) => {
         this.listCocktails = cocktails;
@@ -28,7 +37,11 @@ export class ListCocktails {
       error: (error) => {
         console.error(error);
       },
-      complete: () => {},
+      complete: () => {
+            //this.searched = false;
+            this.loadCocktails = true;
+
+      },
     });
   }
 }
