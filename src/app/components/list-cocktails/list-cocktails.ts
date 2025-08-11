c04fd6fd6a5f87338567886a8efa33575fa8e8fb
celoplay <celoaizaga@gmail.com>
2025-08-11 09:19:42 +0200

    mostrar cockteles

--- src/app/components/list-cocktails/list-cocktails.ts
+++ src/app/components/list-cocktails/list-cocktails.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IFilter } from '../../models/filter.model';
 import { CocktailService } from '../../services/cocktail-service';
+import { ICocktail } from '../../models/cocktel.model';
 
 @Component({
   selector: 'app-list-cocktails',
@@ -11,6 +12,7 @@ import { CocktailService } from '../../services/cocktail-service';
 })
 export class ListCocktails {
   private cocktailService = inject(CocktailService);
+  public listCocktails: ICocktail[]=[];
 
   public filter: IFilter = {
     searchBy: 'name',
@@ -20,8 +22,9 @@ export class ListCocktails {
   filterData() {
     this.cocktailService.getCocktails(this.filter).subscribe({
 
-      next: (data: any)=> {
-        console.log(data);
+      next: (cocktails: ICocktail[])=> {
+        //console.log(ListCocktails)
+        this.listCocktails = cocktails;
       },
       error:(error)=>{
         console.error(error)
--- src/app/services/cocktail-service.ts
+++ src/app/services/cocktail-service.ts
@@ -2,17 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { IFilter } from '../models/filter.model';
 import { first, map } from 'rxjs';
+import { ICocktail } from '../models/cocktel.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CocktailService {
-
-  private URL_BASE = "https://www.thecocktaildb.com/api/json/v1/1/";
+  private URL_BASE = 'https://www.thecocktaildb.com/api/json/v1/1/';
   private http = inject(HttpClient);
 
-  getCocktails(filter:IFilter){
-    
+  //Devuelve todas las propiedades no vacías cuyo nombre empieza por `property`
+  private parseArray(drink: any, property:string):string[]{
+    return Object.keys(drink)
+    .filter(key =>key.startsWith(property) && drink[key])
+    .map(key => drink[key] as string)
+  }
+
+  getCocktails(filter: IFilter) {
     let additionalURL = '';
 
     if (filter.searchBy == 'name') {
@@ -20,15 +26,38 @@ export class CocktailService {
     } else {
       additionalURL = 'filter.php?';
       if (filter.searchBy == 'glass') {
-        additionalURL+='g=';
-      } else if(filter.searchBy == 'category'){
-        additionalURL+= 'c=';
-      }else{
-        additionalURL+= 'i=';
+        additionalURL += 'g=';
+      } else if (filter.searchBy == 'category') {
+        additionalURL += 'c=';
+      } else {
+        additionalURL += 'i=';
       }
     }
-    additionalURL+=filter.value     
+    additionalURL += filter.value;
     return this.http.get(this.URL_BASE + additionalURL).pipe(
-      first(),map((data:any)=> data));              
-  } 
+      first(),
+      map((data: any) => data)
+    );
+  }
+
+  private parseDrinks(data: any) {
+    if (!data) return [];
+
+    const drinks = ['drinks'] as any[];
+    
+
+    return drinks.map((drink) => {
+      return {
+        id: drink['idDrink'],
+        name: drink['strDrink'],
+      glass: drink['strGlass'],
+      img: drink['strDrinkThumb'],
+      instructions:drink['strInstructionsES']||drink['strInstructions'],
+      ingredients:this.parseArray(drink,'strIngredient'),
+      measures:this.parseArray(drink,'strMeasure')
+    } as ICocktail;
+    });
+  }
+
+
 }
