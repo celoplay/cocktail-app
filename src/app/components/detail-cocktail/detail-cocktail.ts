3f8e28c89a5bfaa3bd5b223ddb3f411ee4ca81bd
celoplay <celoaizaga@gmail.com>
2025-08-12 15:15:19 +0200

    obtener id

--- src/app/components/detail-cocktail/detail-cocktail.ts
+++ src/app/components/detail-cocktail/detail-cocktail.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core/primitives/di';
+import { ActivatedRoute, Params } from '@angular/router';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-detail-cocktail',
   imports: [],
   templateUrl: './detail-cocktail.html',
-  styleUrl: './detail-cocktail.scss'
+  styleUrl: './detail-cocktail.scss',
 })
 export class DetailCocktail {
+  private activeRoute = inject(ActivatedRoute);
 
+  ngOnInit() {
+    this.activeRoute.params.pipe(first()).subscribe({
+      next: (params: Params) => {
+        const id = params['id'];
+        console.log(id);
+      },
+    });
+  }
 }
